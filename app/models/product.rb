0ffd7fbcbb538ca6228836357efac87a9be2f2e8
Daniel Ross <daniel.je.ross@gmail.com>
2012-08-13 22:03:05 +0200

    Add button and correct typos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
 	default_scope :order => 'title'
-        has_many :line_items
-        before_destroy :ensure_not_referenced_by_ay_line_item
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
 	# validation stuff
 	validates :title, :description, :image_url, :presence => true
@@ -15,7 +15,7 @@ class Product < ActiveRecord::Base
   private
 
   # ensure that there are nor line items referencing this product
-  def ensure_not_referenced_by_ay_line_item
+  def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
     else
