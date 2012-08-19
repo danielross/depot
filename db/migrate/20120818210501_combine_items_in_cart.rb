f76960507ee4d985cc4c55d2e2993ad279ccbd3a
Daniel Ross <daniel.je.ross@gmail.com>
2012-08-19 09:53:24 +0200

    Migrate up and down

--- db/migrate/20120818210501_combine_items_in_cart.rb
+++ db/migrate/20120818210501_combine_items_in_cart.rb
@@ -1,4 +1,5 @@
 class CombineItemsInCart < ActiveRecord::Migration
+  
   def up
     # replace multiple items for a single product in a cart with a single item
     Cart.all.each do |cart|
@@ -7,10 +8,10 @@ class CombineItemsInCart < ActiveRecord::Migration
 
       sums.each do |product_id, quantity|
         if quantity > 1 
-          #remove individual items
+          # remove individual items
           cart.line_items.where(:product_id=>product_id).delete_all
 
-          #replace with a single item
+          # replace with a single item
           cart.line_items.create(:product_id=>product_id, :quantity=>quantity)
         end
       end
@@ -18,5 +19,15 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def down
+    # split items with quantity>1 into multiple items
+    LineItem.where("quantity>1").each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create :cart_id=>line_item.cart_id, :product_id=> line_item.product_id, :quantity => 1
+      end
+      #remove original item
+      line_item.destroy
+    end
   end
+
 end
